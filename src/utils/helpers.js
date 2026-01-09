c5c65574502938594e43c209e2a3048c5a4a9232
koolits <ctndg.mmr@gmail.com>
2026-01-09 17:47:43 +0800

    print

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -407,12 +407,7 @@ export const printReceipt = (order) => {
 };
 
 // Helper function for currency formatting (if not already in your utils)
-const formatCurrency = (amount) => {
-  return new Intl.NumberFormat('en-PH', {
-    style: 'currency',
-    currency: 'PHP'
-  }).format(amount);
-};
+
 
 export const groupBy = (array, key) => {
   return array.reduce((result, currentValue) => {
